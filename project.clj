281d7f14c186ee37887b21c8ce0b32ff3f182198
Sergey Sobko <SSobko@ptsecurity.com>
2017-10-09 21:25:23 +0300

    Fix Travis-CI builds

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ptsecurity/flower "0.1.1-SNAPSHOT"
+(defproject com.ptsecurity/flower "0.1.2"
   :description "Flower is a library for integration with task trackers, repositories, messaging systems and more"
   :url "http://github.com/PositiveTechnologies/flower"
   :license {:name "MIT License"
