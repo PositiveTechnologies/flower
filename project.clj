89ea9d95136eadd9706e9e5423a37f0a3086f377
Sergey Sobko <SSobko@ptsecurity.com>
2018-02-13 22:05:10 +0300

    Add deploy-all alias and update LICENSE

--- project.clj
+++ project.clj
@@ -6,14 +6,14 @@
   :plugins [[lein-sub "0.3.0"]
             [lein-bump-version "0.1.6"]]
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.namespace "0.2.11"]
                  [flower/flower-tracker "0.4.0-SNAPSHOT"]
                  [flower/flower-repository "0.4.0-SNAPSHOT"]
                  [flower/flower-messaging "0.4.0-SNAPSHOT"]
                  [flower/flower-team "0.4.0-SNAPSHOT"]
                  [flower/flower-utilities "0.4.0-SNAPSHOT"]]
   :aliases {"test" ["sub" "lint-and-test-all"]
-            "bump-all" ["do" ["bump-version"] ["sub" "bump-version"]]}
+            "bump-all" ["do" ["bump-version"] ["sub" "bump-version"]]
+            "deploy-all" ["do" ["sub" "deploy" "clojars"] ["deploy" "clojars"]]}
   :sub ["flower-proto"
         "flower-common"
         "flower-integration-github"
@@ -29,4 +29,5 @@
         "flower-utilities"
         "lein-template"]
   :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}})
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]}})
